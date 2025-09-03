27536fd350a96c2dd75dfbca46a6c85eb37add55
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-09-03 19:33:27 +0530

    basic , introduction

---
+++ basic/basic1.py
@@ -0,0 +1,33 @@
+# print("Hello")
+# print("Pranshul")
+# print("Python")
+
+# print("hello",end=" ")
+
+# print("pranshul")
+
+# Data type : int , float , str , bool
+
+# a = 10
+# print("value of a = ",a)
+# print(type(a))
+# print(type(a).__name__)
+
+# b = 10.12
+# print(b)
+
+
+# c = 'ram'
+# print(c)
+# print(type(c).__name__)
+
+
+# isAlive = False 
+# print(isAlive)
+# print(type(isAlive).__name__)
+
+
+# a = 10
+
+# print("value of a = ",a)
+# print(f"value of a = {a}")
